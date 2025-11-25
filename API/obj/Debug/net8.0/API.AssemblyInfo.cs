b57ade206ba6aaffec31eaa536e89c559eda651f
LEONARDO MORETTI FERNANDES NABARRO <leonardo.nabarro@cs.up.edu.br>
2025-11-24 21:05:04 -0300

    fixing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b331162934e2a8ef1c1c8532a81667e5974167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b2acb68cb12a2d743cc18f8dfa84be07d4b780")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
